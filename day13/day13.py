4e2b1c56411a3607bae020a58e856c21164ecbd2
Peter Heckel <heckel.pete@gmail.com>
2023-12-20 22:32:15 -0700

    Broken implementation of day13

---
+++ day13/day13.py
@@ -0,0 +1,71 @@
+import sys
+from pathlib import PosixPath
+
+def build_mirror_maps( mirror_rock_map: str ):
+    mirrors = [[]]
+    with open(mirror_rock_map) as f: 
+        mirror_count = 0
+        for line in f:
+            if line == '':
+                mirror_count += 1
+                mirrors[mirror_count] = []
+            else:
+                mirrors[mirror_count].append(list(line.strip()))
+    return mirrors
+
+
+def summarize_mirror( mirror_map: list ):
+    # First check for vertical mirror
+    vertical_split_potentials = set(range(1, len(mirror_map[0])-1))
+    horizontal_split_potentials = set(range(1, len(mirror_map)-1))
+
+    for row in mirror_map:
+        new_potential_v_split = []
+        for split in vertical_split_potentials:
+            left_side = row[:split]
+            right_side = row[split:]
+            left_side = left_side.reverse()
+            if all([x==y for x,y in zip(left_side,right_side)]):
+                new_potential_v_split.append(split)
+        
+        vertical_split_potentials = vertical_split_potentials & set(new_potential_v_split) 
+
+        if len(vertical_split_potentials) == 0:
+            break
+
+    horizontal_split = None
+    # Then check for horizontal spanning mirror
+    for split in horizontal_split_potentials:
+        top_side = mirror_map[:split]
+        bottom_side = mirror_map[split:]
+        top_side = top_side.reverse()
+        if all([x==y for x,y in zip(top_side,bottom_side)]):
+            horizontal_split = split
+
+    if ((horizontal_split is None and len(vertical_split_potentials) == 0) or
+       (horizontal_split is not None and len(vertical_split_potentials) > 0)):
+        assert(False)
+    
+    if horizontal_split:
+        return 100* horizontal_split
+    else:
+        return vertical_split_potentials[0]
+        
+
+
+if __name__ == '__main__':
+    if (len(sys.argv) != 2) or not PosixPath(sys.argv[1]).is_file() :
+        print('Error 1 file argument needed')
+        exit(1)
+
+    # part 1
+    mirror_maps = build_mirror_maps(sys.argv[1])
+
+    mirror_summary = 0
+
+    for mirror_map in mirror_maps:
+        mirror_summary += summarize_mirror( mirror_map )
+
+    print(f'Mirror summary: {mirror_summary}')
+
+    exit(0)
