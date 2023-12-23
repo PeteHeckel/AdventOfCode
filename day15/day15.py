014f861390f17da05239de3a017a930caf662982
Peter Heckel <heckel.pete@gmail.com>
2023-12-22 21:01:43 -0700

    Day15 part 2

--- day15/day15.py
+++ day15/day15.py
@@ -27,3 +27,34 @@ if __name__ == '__main__':
         hash_sum += holiday_ascii_string_helper(input.strip())
     print(f'Hashing Sum {hash_sum}')
 
+
+    # Part 2
+    boxes = {}
+    for box_num in range(256):
+        boxes[box_num] = {}
+
+    for input in inputs:
+        input = input.strip()
+        split_idx = input.find('-')
+        if split_idx != -1:
+            operator = '-'
+            focal_length = None
+        else:
+            split_idx = input.find('=')
+            operator = '='
+            focal_length = input[split_idx+1]
+        lens_label = input[:split_idx]
+        box_label = holiday_ascii_string_helper(lens_label)
+
+        if operator == '=':
+            assert(focal_length is not None)
+            boxes[box_label][lens_label] = int(focal_length)
+        elif boxes[box_label].get(lens_label):
+            boxes[box_label].pop(lens_label)
+
+    focusing_power = 0
+    for box_mult, box in enumerate(boxes.values(), start=1):
+        for lens_mult, lens in enumerate(box.values(),start=1):
+            focusing_power += box_mult * lens_mult * int(lens)
+
+    print(f'Focusing power of the lens config: {focusing_power}')
