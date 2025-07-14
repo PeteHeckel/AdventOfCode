4479da69d3070ed9132710f1a540ae1a7f788138
Peter Heckel <heckel.pete@gmail.com>
2025-07-13 21:54:08 -0600

    Day7 part2

--- python2024/day7.py
+++ python2024/day7.py
@@ -19,28 +19,51 @@ def parse_input( input: str):
         output.append((int(test_value), cal_values))
     return output
 
-def check_operands_possible( output: int, input_vals: list[int] ) -> bool:
+def create_perms( perm_list: list[str], size: int, include_comb: bool ) -> list[str]:
+    if not perm_list:
+        perm_list = ["+", "*"]
+        if include_comb: perm_list.append("|")
+    
+    if len(perm_list[0]) == size:
+        return perm_list
+
+    out_list = []
+    for perm in perm_list:
+        out_list.append(perm + "*")
+        out_list.append(perm + "+")
+        if include_comb: out_list.append(perm + "|")
+
+    return create_perms(out_list, size, include_comb)
+
+def check_operands_possible( output: int, input_vals: list[int], include_combine: bool ) -> bool:
     smallest = sum(input_vals)
     largest = math.prod(input_vals)
 
+    operator_num = len(input_vals) - 1 
+    perms = create_perms([], operator_num, include_combine)
+
     if output == smallest or output == largest:
         return True
-    
-    bit_multer = 0
-    while bit_multer.bit_length() < (len(input_vals)):
-        input_copy = input_vals.copy()
-        value = input_copy.pop(0)
 
-        bit_multer_tester = bit_multer
-        while input_copy:
-            if bit_multer_tester & 1:
-                value *= input_copy.pop(0)
+    for operators in perms:
+        tester_arr = input_vals.copy()
+        value = tester_arr.pop(0)
+
+        for op in operators:
+            num = tester_arr.pop(0)
+            if op == "*":
+                value *= num
+            elif op == "+":
+                value += num
+            elif op == "|":
+                numSize = len(str(num))
+                value = value * (10 ** numSize) + num
             else:
-                value += input_copy.pop(0)
-            bit_multer_tester = bit_multer_tester >> 1
+                assert(False)   # confirm we've parsed correctly
+        assert(not tester_arr)  # confirm it's empty
+
         if value == output:
             return True
-        bit_multer += 1
 
     return False
 
@@ -55,8 +78,14 @@ if __name__ == "__main__":
     sum_of_valid = 0
 
     for (test_val, cal_values) in calibration_list:
-        ops = check_operands_possible( test_val, cal_values )
-        if ops:
-            sum_of_valid += test_val * ops
-    
+        if check_operands_possible( test_val, cal_values, False ):
+            sum_of_valid += test_val
+
     print(f"Valid calibration results: {sum_of_valid}")
+
+    sum_of_valid = 0
+    for (test_val, cal_values) in calibration_list:
+        if check_operands_possible( test_val, cal_values, True ):
+            sum_of_valid += test_val
+
+    print(f"Valid calibration results with combinations: {sum_of_valid}")
